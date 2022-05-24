dec41cd71370d5f7ff9267a5e68133dcabb2f594
Rezy <onlyrezystor@mail.ru>
2022-05-24 14:09:52 +0300

    We changed everything and we get some ideas for future

--- app/src/main/java/com/example/lavaro_prod/MainActivity.java
+++ app/src/main/java/com/example/lavaro_prod/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.lavaro_prod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean checker_of_registration = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+        if (checker_of_registration){
+//            Intent shtuka
+        }
     }
 }
